cb4716c316d68b7ac1042a7fca0911e98847573c
DexArchLaptop <143239271+BattleDemon@users.noreply.github.com>
2026-05-15 13:19:45 +1000

    EEGEG

--- Code/Testing/IRcircle.py
+++ Code/Testing/IRcircle.py
@@ -7,6 +7,7 @@ from pybricks.ev3devices import (
 )
 from pybricks.parameters import Port, Button, Color
 from pybricks.tools import wait
+import time
 
 from pybricks.iodevices import I2CDevice
 
@@ -14,3 +15,16 @@ ev3 = EV3Brick()
 
 ir_sensor = I2CDevice(Port.S4,0x08)
 
+ev3.screen.print("EV3 TEST READY")
+
+time.sleep(0.5)
+
+while True:
+    ev3.screen.clear()
+    ball_sensor_data = ir_sensor.read(2,2)
+
+    ball_position = ball_sensor_data[0]
+    ball_sig_strength = ball_sensor_data[1]
+
+    ev3.screen.print(ball_position)
+    time.sleep(1)
