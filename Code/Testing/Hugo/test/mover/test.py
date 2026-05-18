4ceb3ee7f2998720bb0751a010f6cde7b0425dc6
BlackFire26299 <130209448+BlackFire26299@users.noreply.github.com>
2026-05-18 14:14:35 +1000

    testing for the defensive robot
    Tested movement and the throwing ability. Throwing is almost done and ready to be moved on towards the final stages

---
+++ Code/Testing/Hugo/test/mover/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pybricks-micropython
+
+from time import sleep, time
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (
+    Motor,
+    TouchSensor,
+    ColorSensor,
+    InfraredSensor,
+    UltrasonicSensor,
+    GyroSensor,
+)
+import random
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile, Font
+from math import pi, tan, sin, cos
+import os
+import threading
+
+
+ev3 = EV3Brick()
+
+motorL = Motor(Port.C)
+motorR = Motor(Port.D)
+pusher = Motor(Port.A)
+
+motorL.run(1000)
+motorR.run(1000)
+sleep(2)
---
+++ Code/Testing/Hugo/test/thrower/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pybricks-micropython321  
+
+from time import sleep, time
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (
+    Motor,
+    TouchSensor,
+    ColorSensor,
+    InfraredSensor,
+    UltrasonicSensor,
+    GyroSensor,
+)
+import random
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile, Font
+from math import pi, tan, sin, cos
+import os
+import threading
+
+IN = -2000
+OUT = 2000
+
+ev3 = EV3Brick()
+
+motor = Motor(Port.B)
+motor2 = Motor(Port.C)
+cs = ColorSensor(Port.S4)
+ballwasthere = False
+while True:
+    buttons = ev3.buttons.pressed()
+    if cs.color() == Color.BLACK:
+        motor.run(OUT)
+        motor2.run(OUT)
+        ballwasthere = True
+    else:
+        if ballwasthere:
+            sleep(1)
+            ballwasthere=False
+        motor.run(IN)
+        motor2.run(IN)
