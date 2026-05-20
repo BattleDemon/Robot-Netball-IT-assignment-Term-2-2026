e81515a6b84f768638956fee5a841cddec3871e9
DexArchLaptop <143239271+BattleDemon@users.noreply.github.com>
2026-05-20 13:00:55 +1000

    Create Attack.py

---
+++ Code/ATTACK/Attack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pybricks-micropython
+
+''' This File is a combination of Zen and Gabe's work '''
+
+# Import things from the other files so we can use them from one file
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (
+    Motor, TouchSensor, ColorSensor,
+    InfraredSensor, UltrasonicSensor, GyroSensor
+)
+from pybricks.parameters import Port, Button, Color
+from pybricks.tools import wait
+
+from pybricks.iodevices import I2CDevice
+
+# Non Ev3 Imports
+from enum import Enum
+from threading import *
+import os
+import time
+import random
+
+# States
+class State(Enum):
+    pass
+
+# State Machine
+class State_Machine():
+    pass
+
+class Defender(): 
+    def __init__(self):
+        pass
