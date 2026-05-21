036fe0d7238cd9376992d4fc77e78b72121d8cd5
DexArchLaptop <143239271+BattleDemon@users.noreply.github.com>
2026-05-21 10:58:26 +1000

    Update Foul_Controller.py

--- Code/DEFENCE/Foul_Controller.py
+++ Code/DEFENCE/Foul_Controller.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 
+import time
+
 ''' Work of Dexter '''
 
 #User Presses touch sensor once (tells machine it is being moved to foul)
@@ -13,13 +15,28 @@
 # return to normal state
 
 class Foul_controller():
-    def __init__(self,colour_sensor):
+    def __init__(self, owner, colour_sensor):
         self.colour_sensor = colour_sensor
         
-        self.see_ground_thread = Thread(target=see_ground)
+        self.observe_ground_thread = Thread(target=observe_ground)
+        self.observe_ground_thread.daemon = True
+        self.observe_ground_thread.start()
 
-    def see_ground(self):
-        pass
+        self.observed_colour = None
+
+        self.currently_foul = False
+
+    def observe_ground(self):
+        while True:
+            self.observed_colour = self.colour_sensor.color_name()
+
+            if self.colour_sensor == "White" and self.currently_foul == False:
+                self.currently_foul = True
+                foul()
+
+            ## Checks for tape on ground that send to owner
+
+            time.sleep(0.75)
 
     def foul(self):
         pass
