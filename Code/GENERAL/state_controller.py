bb64b4417084da6053a2a0533a221f83bc23769f
DexterH <DexterHart0@outlook.com>
2026-05-24 09:33:36 +1000

    working on state manager

--- Code/GENERAL/state_controller.py
+++ Code/GENERAL/state_controller.py
@@ -1,15 +1,52 @@
+#!/usr/bin/env pybricks-micropython
 
+from enum import Enum
 
 # States
-class State():
-    IDLE = 1
-    FOUL = 2
-    PASSING = 3
-    RETRIEVING = 4
-    LOCATING = 5
-    POSITIONING = 6
+class State(Enum):
+    IDLE = 0
+    FOUL = 1
+    PASSING = 2
+    RETRIEVING = 3
+    LOCATING = 4
+    POSITIONING = 5
+    RECIEVING = 6
+
+class Request(Enum):
+    PASS = 0
+    RECIEVE = 1
+    RETRIEVE = 2
+    NONE = 3
+
 
 # State Machine
 class State_Controller():
     def __init__(self):
         self.state = State.IDLE
+        self.other_state: State
+
+        self.x_pos: float
+        self.y_pos: float
+        self.angle: float
+
+        self.position: tuple
+        self.other_position: tuple
+
+        self.ball_position: tuple
+        self.other_ball_position: tuple
+
+        self.has_ball: bool
+        self.other_has_ball: bool
+
+        self.request: Request
+        self.incoming_request: Request
+
+    def update_self_position(x, y, angle):
+        pass
+
+    def update_other_position(x, y, angle):
+        pass
+
+    def get_snapshot():
+        # Gather all info the communication manager needs
+        pass
