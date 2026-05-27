6002c5e3d7c9616e019b171d02bd514fa6833bd6
DexterH <DexterHart0@outlook.com>
2026-05-27 21:59:19 +1000

    Update PushAndAim.py

---
+++ Code/DEFENCE/PushAndAim.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pybricks-micropython
+
+
+
+from math import atan2, degrees
+from pybricks.ev3devices import Motor
+
+class PushAndShoot():
+    def __init(self):
+        pass
+
+    def aim(self):
+        pass
+
+    def shoot(self):
+        pass
