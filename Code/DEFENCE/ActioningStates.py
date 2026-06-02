602cd0f8486be5ecb76f92a8255ab27c6ff72b87
BlackFire26299 <130209448+BlackFire26299@users.noreply.github.com>
2026-06-02 13:48:39 +1000

    now the main loop

--- Code/DEFENCE/ActioningStates.py
+++ Code/DEFENCE/ActioningStates.py
@@ -20,7 +20,7 @@ import os
 import threading
 
 from GENERAL import movement,IRlocation
-from GENERAL.StateController import State_Controller
+from GENERAL.StateController import State_Controller, State
 
 from DEFENCE.PushAndAim import PushAndAim
 
@@ -28,33 +28,48 @@ from DEFENCE.PushAndAim import PushAndAim
 class StateActions:
     def __init__(self, PushMotor: Motor, StateController: State_Controller, driver: movement.Driver ) -> None:
         self.PushMotor = PushMotor
-        self.State = StateController
+        self.StateController = StateController
         self.Driver = driver
         self.pushingCode = PushAndAim(self.PushMotor)
 
     #When foul ends
-
+    def EndOfFoul(self):
+        self.Driver.home_from_foul_box()
     # Passing
     def Passing(self):
         
-        angleToTeamMate = self.pushingCode.get_aim_angle(self.State.position, self.State.others_position)
+        angleToTeamMate = self.pushingCode.get_aim_angle(self.StateController.position, self.StateController.others_position)
         self.Driver.pivot("LEFT") # NEED GABE TO CODE A PIVOT BY ANGLE
         self.pushingCode.push()
     
     # retrieving
         #NEEDS ZENS TRIANGULATION CODE
+        
     # Locating
     def Locating(self):
         self.Driver.turn_angle(10)
+        
 
     # Positioning
     def Positioning(self):
-        pass
+        self.Driver.drive_to_point(100,100)
 
 
     # Receiving
         # Get ready to receive the ball from your teammate
+    def Receiving(self):
+        angleToTeamMate = self.pushingCode.get_aim_angle(self.StateController.position, self.StateController.others_position)
+        self.Driver.pivot("LEFT") # NEED GABE TO CODE A PIVOT BY ANGLE
+
 
     #Waiting
+    def Waiting(self):
+        pass
 
     #Main loop
+    def MainLoop(self):
+        while True:
+            Current_state = self.StateController.get_state()
+            
+            
+            
