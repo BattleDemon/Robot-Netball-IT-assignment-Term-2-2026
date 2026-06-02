42ff73e5dd3da0219b7bde85ec1296d1a157a8b0
BlackFire26299 <130209448+BlackFire26299@users.noreply.github.com>
2026-06-02 11:25:27 +1000

    created file

---
+++ Code/DEFENCE/ActioningStates.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pybricks-micropython
+
+from time import sleep, time
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (
+    Motor,
+    TouchSensor,
+    ColorSensor,
+    InfraredSensor,
+    UltrasonicSensor,
+    GyroSensor,
+)
+import random
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile, Font
+from math import pi, tan, sin, cos
+import os
+import threading
