124628dd2596f809b60339fd4dab11cc143ef6b4
BlackFire26299 <130209448+BlackFire26299@users.noreply.github.com>
2026-06-03 21:34:58 +1000

    Main file rewritten

--- Code/DEFENCE/DefenceMain.py
+++ Code/DEFENCE/DefenceMain.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env pybricks-micropython
 
-# ++++++++++++++++++++++++++++++++*********
-# ======== Work of Hugo and Dexter ========
-# ++++++++++++++++++++++++++++++++*********
+# ++++++++++++++++++++++++++++++++**********
+# ==== Work of Hugo (started by dexter) ====
+# ++++++++++++++++++++++++++++++++**********
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (
@@ -32,5 +32,51 @@ from DEFENCE.ActioningStates import StateActions
 # Main Robot class for the defending robot
 class Defender(): 
     def __init__(self):
+        
 
-        pass
+        self.ev3 = EV3Brick()
+        self.team = 'defence'
+        self.StateController = State_Controller(self,self.team, 0,0,0,0,0,0,0)
+
+        self.leftMotor = Motor(Port.B)
+        self.rightMotor = Motor(Port.C)
+        self.pushMotor = Motor(Port.D)
+
+        self.GroundDetectionSensor = ColorSensor(Port.S1)
+        self.BallSensor = ColorSensor(Port.S4)
+        self.gyro = GyroSensor(Port.S2)
+
+
+        self.communicator = Communicator(self.StateController, self.team, self.ev3)
+        self.communicationThread = Thread(target=self.communicator.CommunicationLoop)
+        self.communicationThread.start()
+
+        self.ballSensorThread = Thread(target=self.ball_sensing)
+        self.ballSensorThread.start()
+
+        self.Driver = Driver(self.ev3, self.leftMotor,self.rightMotor,self.GroundDetectionSensor,self.team, self.gyro)
+
+
+        self.stateActioner = StateActions(self.pushMotor, self.StateController, self.Driver)
+
+        self.groundObserver = Ground_Observer(self.StateController, self.GroundDetectionSensor)
+
+        #IR detection initialisation code goes here.
+
+
+        self.Start()
+        
+    def ball_sensing(self):
+        while True:
+            if self.BallSensor.color() == Color.BLACK:
+                self.has_ball = True
+                while True:
+                    if self.BallSensor.color() != Color.BLACK:
+                        break
+                time.sleep(0.5)
+            time.sleep(0.5)
+
+    def Start(self):
+        while True:
+            self.ev3.screen.print("WEEEE I am defending")
+            time.sleep(0.2)
