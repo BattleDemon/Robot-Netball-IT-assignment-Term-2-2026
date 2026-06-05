f6894805a77d8f67406cdaff7dc3201ed8eb3efe
BlackFire26299 <130209448+BlackFire26299@users.noreply.github.com>
2026-06-05 13:17:46 +1000

    awdwd

--- Code/DefenceMain.py
+++ Code/DefenceMain.py
@@ -23,8 +23,7 @@ import random
 from GENERAL.IRlocation import irLocator
 from GENERAL.StateController import State, State_Controller
 from GENERAL.movement import Driver
-from GENERAL.irsensor_triangulation import ir_controller
-import CatchAndThrow
+
 from GENERAL.GroundDetectionSystem import Ground_Observer
 from GENERAL.communication import Communicator
 from DEFENCE.ActioningStates import StateActions
